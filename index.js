b84760fe5afbbe4acb790268aabeec9bdb9d8132
BoulghaitZakaria <61053305+BoulghaitZakaria@users.noreply.github.com>
2021-03-29 16:42:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function test(){
-    console.log('this is a test');
+    console.log('this is the test 6');
     console.log('about get element by tag name');
-    var element = document.getElementsByTagName("section"), index;
-    for (index = element.length - 1; index >= 0; index--) {
-        element[index].parentNode.removeChild(element[index]);
-    }
+    var element = document.getElementsByTagName("section");
+    element.remove();
 }
