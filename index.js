f517388b4771d78eb85acf9df4eefbdd08974f7c
BoulghaitZakaria <61053305+BoulghaitZakaria@users.noreply.github.com>
2021-03-29 16:33:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function test(){
     console.log('this is a test');
     let element = document.getElementsByClassName('slds-modal_prompt');
-    parentNode.removeChild(element);
-    element.remove();
+    if(element){
+     parentNode.removeChild(element);
+        element.remove();
+    }
     console.log('end');
 }
